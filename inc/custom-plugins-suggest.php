e397e4d0e25318624e80c7843a2fa566619b9273
Graeme <graeme.a.i.reed@gmail.com>
2019-04-14 17:49:49 -0600

    added TGM Plugin Activation to suggest theme plugins

---
+++ inc/custom-plugins-suggest.php
@@ -0,0 +1,28 @@
+<?php
+ 
+/**
+ * Since I'm already doing a tutorial, I'm not going to include comments to
+ * this code, but if you want, you can check out the "example.php" file
+ * inside the ZIP you downloaded - it has a very detailed documentation.
+ */
+ 
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
+ 
+add_action( 'tgmpa_register', 'understrap_require_plugins' );
+ 
+function understrap_require_plugins() {
+ 
+    $plugins = array(
+        array(
+            'name'      => 'Advanced Custom Fields',
+            'slug'      => 'advanced-custom-fields',
+            'required'  => false, // this plugin is recommended
+        )
+    );
+    $config = array( /* The array to configure TGM Plugin Activation */ );
+ 
+    tgmpa( $plugins, $config );
+ 
+}
+ 
+?>
