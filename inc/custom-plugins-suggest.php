3ed716ad9b859db04e56184f42803ca2dcf7bb13
Graeme Reed <graeme@parcomonline.com>
2019-04-16 11:28:24 -0600

    added ACF Pro plugin source files to theme. updated suggested plugin list.

--- inc/custom-plugins-suggest.php
+++ inc/custom-plugins-suggest.php
@@ -1,33 +1,49 @@
 <?php
- 
+
 /**
  * Since I'm already doing a tutorial, I'm not going to include comments to
  * this code, but if you want, you can check out the "example.php" file
  * inside the ZIP you downloaded - it has a very detailed documentation.
  */
- 
+
 require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
- 
+
 add_action( 'tgmpa_register', 'understrap_require_plugins' );
- 
+
 function understrap_require_plugins() {
- 
+
     $plugins = array(
         array(
             'name'      => 'Advanced Custom Fields',
             'slug'      => 'advanced-custom-fields',
-            'required'  => false, // this plugin is recommended
+            'required'  => true, // this plugin is recommended
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
         ),
         array(
-            'name'      => 'Block Lab',
-            'slug'      => 'block-lab',
-            'required'  => false, // this plugin is recommended
-        )
+			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
+			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+			'source'             => get_stylesheet_directory() . '/lib/plugins/advanced-custom-fields-pro.5.8.0-RC2.zip', // The plugin source.
+			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+			'version'            => '5.8.0-RC2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
+        array(
+            'name'      => 'Font Awesome',
+            'slug'      => 'font-awesome',
+            'required'  => true, // this plugin is recommended
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+        ),
+
     );
-    $config = array( /* The array to configure TGM Plugin Activation */ );
- 
+    $config = array(
+        'is_automatic' => true,
+    );
+
     tgmpa( $plugins, $config );
- 
+
 }
- 
+
 ?>
