e7609220d68596e7c34acfef9e1edc22d2970197
Graeme Reed <graeme@parcomonline.com>
2019-06-19 12:00:05 -0600

    updated ACF Pro pluggin file

--- inc/custom-plugins-suggest.php
+++ inc/custom-plugins-suggest.php
@@ -22,9 +22,9 @@ function evstrap_require_plugins() {
         array(
 			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
 			'slug'               => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory_uri() . '/lib/plugins/advanced-custom-fields-pro.5.8.0-RC2.zip', // The plugin source.
+			'source'             => get_template_directory_uri() . '/lib/plugins/advanced-custom-fields-pro.5.8.1.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '5.8.0-RC2', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+			'version'            => '5.8.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
 			'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
